55690c3b4e09cf4cc606e400ed55011dfa7b2868
tmarshall <timothyjmarshall@gmail.com>
2018-04-22 17:23:24 -0700

    run lints serial

--- dist/lib/start-process.js
+++ dist/lib/start-process.js
@@ -2,11 +2,14 @@
 
 var _crossSpawn = require("cross-spawn");
 
-module.exports = function startProcess(_ref) {
+module.exports = function startProcess(_ref, onClose) {
   var _ref$command = _ref.command,
       command = _ref$command === void 0 ? 'node' : _ref$command,
       _ref$args = _ref.args,
       args = _ref$args === void 0 ? [] : _ref$args;
+
+  onClose = onClose || function () {};
+
   var proc = (0, _crossSpawn.spawn)(command, args, {
     stdio: 'inherit',
     cwd: __dirname
@@ -18,17 +21,21 @@ module.exports = function startProcess(_ref) {
 
     if (signal) {
       if (signal === 'SIGKILL') {
+        onClose(137);
         process.exit(137);
       }
 
       console.log("got signal ".concat(signal, ", exiting"));
+      onClose(1);
       process.exit(1);
     }
 
+    onClose(0);
     process.exit(0);
   });
   proc.on('error', function (err) {
     console.error(err);
+    onClose(1);
     process.exit(1);
   });
   return proc;
