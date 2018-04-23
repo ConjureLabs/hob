e15fd111f9406bf2a7bd59083646fcbe6e90a331
tmarshall <timothyjmarshall@gmail.com>
2018-04-22 17:28:23 -0700

    set cwd on lints

--- dist/lib/start-process.js
+++ dist/lib/start-process.js
@@ -6,13 +6,15 @@ module.exports = function startProcess(_ref, onClose) {
   var _ref$command = _ref.command,
       command = _ref$command === void 0 ? 'node' : _ref$command,
       _ref$args = _ref.args,
-      args = _ref$args === void 0 ? [] : _ref$args;
+      args = _ref$args === void 0 ? [] : _ref$args,
+      _ref$cwd = _ref.cwd,
+      cwd = _ref$cwd === void 0 ? __dirname : _ref$cwd;
 
   onClose = onClose || function () {};
 
   var proc = (0, _crossSpawn.spawn)(command, args, {
     stdio: 'inherit',
-    cwd: __dirname
+    cwd: cwd
   });
   proc.on('close', function (code, signal) {
     if (code !== null) {
